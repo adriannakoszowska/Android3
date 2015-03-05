4b53b1c19759e4dffd9727d962b8ffd414378dad
Adrianna Koszowska <adriannakoszowska@gmail.com>
2015-03-05 16:55:26 +0100

    skryptowanie funkcji: arcus sinus, arcus cosinus, arcus tangens, testowanie aplikacji - poprawne wyniki testów

--- app/src/main/java/com/example/adriannakoszowska/android3/MainActivity.java
+++ app/src/main/java/com/example/adriannakoszowska/android3/MainActivity.java
@@ -225,7 +225,6 @@ public class MainActivity extends ActionBarActivity {
         dzialanie_globalne = "";
         dzialanie.setText("");
         poprzednie.setText("");
-
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
